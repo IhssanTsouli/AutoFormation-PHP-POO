8c99cb1e6a4874bddd49349a6ed871b675ade474
ihssan <ihssantsouli@gmail.com>
2023-01-21 19:21:08 +0100

    access modifiers to properties

---
+++ Access Modifiers/properties.php
@@ -0,0 +1,19 @@
+<!-- access modifiers to properties -->
+<?php
+class Fruit {
+  public $name;
+  protected $color;
+  private $weight;
+}
+
+$mango = new Fruit();
+$mango->name = 'Mango'; // OK
+$mango->color = 'Yellow'; // ERROR
+$mango->weight = '300'; // ERROR
+?>
+
+<!-- 
+    public - the property or method can be accessed from everywhere. This is default
+    protected - the property or method can be accessed within the class and by classes derived from that class
+    private - the property or method can ONLY be accessed within the class -->
+
